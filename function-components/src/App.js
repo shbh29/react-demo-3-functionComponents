895215074cc70d904dece3bb43e0bed39967a72d
Shubham Singh <shbh29@gmail.com>
2023-05-21 07:27:12 +0530

    useState one more example

--- function-components/src/App.js
+++ function-components/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import React, {useState} from 'react';
 
 function App () {
-  const [value, setValue] = useState(10);
+  const [value, setValue] = useState(65);
+  const [asciiCode, setAsciiCode] = useState(value);
   return (
     <>
       <h1>{value}</h1> 
+      <h1>{String.fromCodePoint(asciiCode)}</h1> 
       <button onClick={() => {
-        setValue(value + 1)
+        setValue(value + 1);
+        setAsciiCode(asciiCode + 1);
       }}>Increment</button>
     </>
   )
