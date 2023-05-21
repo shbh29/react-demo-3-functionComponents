deb863b7481aa83236f4d044342334694f340650
Shubham Singh <shbh29@gmail.com>
2023-05-21 07:54:48 +0530

    useEffect one more example skeleton

--- function-components/src/App.js
+++ function-components/src/App.js
@@ -2,25 +2,18 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 
 function App() {
-  const [height, setHeight] = useState(window.innerHeight);
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    console.log("useEffect was called");
-    function handler() {
-      setHeight(window.innerHeight);
-      setWidth(window.innerWidth);
-    }
-    window.addEventListener('resize', handler);
-
-    return () => {
-      console.log('clean up was called')
-      window.removeEventListener('resize',handler);
-    }
-  })
-
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('Hello World!');
   return (
-    <h1>{height}, {width}</h1>
+    <>
+      <input type='text' onChange={(event) => {
+        setText(event.target.value);
+      }}></input>
+      <button onClick={() => {
+        setCount(count + 1);
+      }}>Click</button>
+      <h1>{text} - {count}</h1>
+    </>
   )
 }
 
